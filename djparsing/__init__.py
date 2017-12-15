3eb2714bd97834866c3a1f6f87b151f9466b46c1
lincvv <vlinchevskyi@gmail.com>
2017-12-15 20:08:31 +0200

    add setup

--- djparsing/__init__.py
+++ djparsing/__init__.py
@@ -1,2 +1,2 @@
 __author__ = 'lincvv'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from djparsing import __author__, __version__
+
+setup(
+    name='djparsing',
+    version=__version__,
+    packages=['djparsing'],
+    author=__author__,
+    author_email='vlinchevskyi@gmail.com',
+    license='BSD License',
+    url='https://github.com/lincvv/djparsing',
+)
