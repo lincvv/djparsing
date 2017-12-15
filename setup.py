f6b1944a3bea6480c466a41672d4904b866d2280
lincvv <vlinchevskyi@gmail.com>
2017-12-15 21:09:14 +0200

    v0.1.2

--- setup.py
+++ setup.py
@@ -3,10 +3,23 @@ from djparsing import __author__, __version__
 
 setup(
     name='djparsing',
+    description='Convenient parser with saving data to the database',
     version=__version__,
     packages=['djparsing'],
     author=__author__,
     author_email='vlinchevskyi@gmail.com',
     license='BSD License',
     url='https://github.com/lincvv/djparsing',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities',
+    ],
 )
