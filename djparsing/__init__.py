989cf27e874b514fbf95095658448447629737f4
lincvv <vlinchevskyi@gmail.com>
2018-06-30 15:10:26 +0300

    PATH_TEMP for python 3.5

--- djparsing/__init__.py
+++ djparsing/__init__.py
@@ -1,2 +1,2 @@
 __author__ = 'lincvv'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
--- djparsing/settings.py
+++ djparsing/settings.py
@@ -1,8 +1,9 @@
-# import os
+import os
+import sys
 import pathlib
 # from django.conf import settings
 
-# PATH_TEMP = os.path.join(os.path.abspath(os.curdir), 'temp')
-
-
-PATH_TEMP = pathlib.Path.cwd().joinpath('temp')
+if int(sys.version_info.minor) < 6:
+    PATH_TEMP = os.path.join(os.path.abspath(os.curdir), 'temp')
+else:
+    PATH_TEMP = pathlib.Path.cwd().joinpath('temp')
